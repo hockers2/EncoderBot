110e4ae38436ae0ef4c606b38f4dc929550d1a56
Dark Samurai <73326139+ninja7-bot@users.noreply.github.com>
2021-07-15 12:10:39 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 #if os.path.exists('config.env'):
 #  load_dotenv('config.env')
 
-api_id = int(os.environ.get("API_ID"))
-api_hash = os.environ.get("API_HASH")
-bot_token = os.environ.get("BOT_TOKEN")
+api_id = int(os.environ.get('API_ID', None))
+api_hash = os.environ.get('API_HASH', None)
+bot_token = os.environ.get('BOT_TOKEN', None)
 download_dir = os.environ.get("DOWNLOAD_DIR", "downloads/")
-sudo_users = list(set(int(x) for x in os.environ.get("SUDO_USERS").split()))
+sudo_users = list(set(int(x) for x in os.environ.get('SUDO_USERS', None).split()))
 
 app = Client(":memory:", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
